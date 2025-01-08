045752a22ccaa9cc4e53a1f39948ce7b9ab40e9d
ellenfel <ellfelfen@gmail.com>
2025-01-08 07:05:05 +0300

    done with level set up

---
+++ code/tile.py
@@ -0,0 +1,11 @@
+import pygame
+from settings import *
+import os
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(base_path, '../graphics/test/rock.png')
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
