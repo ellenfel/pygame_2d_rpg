e3119cce3df5bef09a1ab8365e10d942d0c155f8
ellenfel <ellfelfen@gmail.com>
2025-08-21 01:01:00 +0300

    added ability to damage and cooldowns

--- code/enemy.py
+++ code/enemy.py
@@ -37,6 +37,11 @@ class Enemy(Entity):
 		self.attack_time = None
 		self.attack_cooldown = 400
 
+		# invincibility timer
+		self.vulnerable = True
+		self.hit_time = None
+		self.invincibility_duration = 300
+
 	def import_graphics(self,name):
 		self.animations = {'idle':[],'move':[],'attack':[]}
 		main_path = f'../graphics/monsters/{name}/'
@@ -88,17 +93,26 @@ class Enemy(Entity):
 		self.image = animation[int(self.frame_index)]
 		self.rect = self.image.get_rect(center = self.hitbox.center)
 
-	def cooldown(self):
+	def cooldowns(self):
+		current_time = pygame.time.get_ticks()
+		
 		if not self.can_attack:
 			current_time = pygame.time.get_ticks()
 			if current_time - self.attack_time >= self.attack_cooldown:
 				self.can_attack = True
 
+		if not self.vulnerable:
+			if current_time - self.hit_time >= self.invincibility_duration:
+				self.vulnerable = True
+
 	def get_damage(self,player,attack_type):
-		if attack_type == 'weapon':
-			self.health -= player.get_full_weapon_damage()
-		else:
-			pass  # Handle magic or other attack types here
+		if self.vulnerable:				
+			if attack_type == 'weapon':
+				self.health -= player.get_full_weapon_damage()
+			else:
+				pass  # Handle magic or other attack types here
+			self.hit_time = pygame.time.get_ticks()
+			self.vulnerable = False
 
 	def check_death(self):
 		if self.health <= 0:
@@ -107,7 +121,7 @@ class Enemy(Entity):
 	def update(self):
 		self.move(self.speed)
 		self.animate()
-		self.cooldown()
+		self.cooldowns()
 		self.check_death()
 
 	def enemy_update(self,player):
